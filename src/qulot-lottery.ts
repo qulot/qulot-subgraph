04a24421eb327a2763f270a728ea648fb82a0f9d
datntz <nguyendat.2741998@gmail.com>
2023-03-28 00:09:38 +0700

    first deploy complete

--- src/qulot-lottery.ts
+++ src/qulot-lottery.ts
@@ -6,7 +6,7 @@ import {
   RoundOpen,
   RoundReward,
   TicketsClaim,
-  TicketsPurchase,
+  TicketsPurchase
 } from "../generated/QulotLottery/QulotLottery";
 import { Lottery, RewardRule, Round, User } from "../generated/schema";
 import { BigInt } from "@graphprotocol/graph-ts";
@@ -14,32 +14,18 @@ import { BigInt } from "@graphprotocol/graph-ts";
 const REWARD_UNITS = ["Percent", "Fixed"];
 
 export function handleNewLottery(event: NewLottery): void {
-  const {
-    lotteryId,
-    verboseName,
-    picture,
-    numberOfItems,
-    minValuePerItem,
-    maxValuePerItem,
-    periodDays,
-    periodHourOfDays,
-    maxNumberTicketsPerBuy,
-    pricePerTicket,
-    treasuryFeePercent,
-    amountInjectNextRoundPercent,
-  } = event.params;
-  const lottery = new Lottery(lotteryId.toString());
-  lottery.verboseName = verboseName;
-  lottery.picture = picture;
-  lottery.numberOfItems = numberOfItems.toI32();
-  lottery.minValuePerItem = minValuePerItem.toI32();
-  lottery.maxValuePerItem = maxValuePerItem.toI32();
-  lottery.periodDays = periodDays.map((periodDay) => periodDay.toI32());
-  lottery.periodHourOfDays = periodHourOfDays.toI32();
-  lottery.maxNumberTicketsPerBuy = maxNumberTicketsPerBuy.toI32();
-  lottery.pricePerTicket = pricePerTicket;
-  lottery.treasuryFeePercent = treasuryFeePercent.toI32();
-  lottery.amountInjectNextRoundPercent = amountInjectNextRoundPercent.toI32();
+  const lottery = new Lottery(event.params.lotteryId);
+  lottery.verboseName = event.params.verboseName;
+  lottery.picture = event.params.picture;
+  lottery.numberOfItems = event.params.numberOfItems.toI32();
+  lottery.minValuePerItem = event.params.minValuePerItem.toI32();
+  lottery.maxValuePerItem = event.params.maxValuePerItem.toI32();
+  lottery.periodDays = event.params.periodDays.map<i32>(periodDay => periodDay.toI32());
+  lottery.periodHourOfDays = event.params.periodHourOfDays.toI32();
+  lottery.maxNumberTicketsPerBuy = event.params.maxNumberTicketsPerBuy.toI32();
+  lottery.pricePerTicket = event.params.pricePerTicket;
+  lottery.treasuryFeePercent = event.params.treasuryFeePercent.toI32();
+  lottery.amountInjectNextRoundPercent = event.params.amountInjectNextRoundPercent.toI32();
 
   lottery.block = event.block.number;
   lottery.timestamp = event.block.timestamp;
@@ -48,12 +34,12 @@ export function handleNewLottery(event: NewLottery): void {
 }
 
 export function handleNewRewardRule(event: NewRewardRule): void {
-  const { lotteryId, matchNumber, rewardUnit, rewardValue } = event.params;
-  const rewardRuleId = lotteryId + ":" + matchNumber.toString();
+  const rewardRuleId = event.params.lotteryId + ":" + event.params.matchNumber.toString();
   const rewardRule = new RewardRule(rewardRuleId);
-  rewardRule.lottery = lotteryId;
-  rewardRule.rewardUnit = REWARD_UNITS[rewardUnit];
-  rewardRule.rewardValue = rewardValue;
+  rewardRule.lottery = event.params.lotteryId;
+  rewardRule.rewardUnit = REWARD_UNITS[event.params.rewardUnit];
+  rewardRule.rewardValue = event.params.rewardValue;
+  rewardRule.matchNumber = event.params.matchNumber.toI32();
 
   rewardRule.block = event.block.number;
   rewardRule.timestamp = event.block.timestamp;
@@ -62,33 +48,31 @@ export function handleNewRewardRule(event: NewRewardRule): void {
 }
 
 export function handleRoundClose(event: RoundClose): void {
-  const { roundId, totalAmount, totalTickets } = event.params;
-  const round = Round.load(roundId.toString());
+  const round = Round.load(event.params.roundId.toString());
   if (round) {
     round.status = "Close";
-    round.totalAmount = totalAmount;
-    round.totalTickets = totalTickets;
+    round.totalAmount = event.params.totalAmount;
+    round.totalTickets = event.params.totalTickets;
     round.save();
   }
 }
 
 export function handleRoundDraw(event: RoundDraw): void {
-  const { roundId, numbers } = event.params;
-  const round = Round.load(roundId.toString());
+  const round = Round.load(event.params.roundId.toString());
   if (round) {
     round.status = "Draw";
-    round.winningNumbers = numbers.map((num) => num.toI32());
+    round.winningNumbers = event.params.numbers.map<i32>(num => num.toI32());
     round.save();
   }
 }
 
 export function handleRoundOpen(event: RoundOpen): void {
-  const { roundId, lotteryId, totalAmount, startTime, drawDateTime } = event.params;
-  const round = new Round(roundId.toString());
-  round.lottery = lotteryId;
-  round.totalAmount = totalAmount;
-  round.startTime = startTime;
-  round.drawDateTime = drawDateTime;
+  const round = new Round(event.params.roundId.toString());
+  round.lottery = event.params.lotteryId;
+  round.totalAmount = event.params.totalAmount;
+  round.totalTickets = BigInt.zero();
+  round.startTime = event.params.startTime;
+  round.drawDateTime = event.params.drawDateTime;
   round.status = "Open";
 
   round.block = event.block.number;
@@ -96,17 +80,16 @@ export function handleRoundOpen(event: RoundOpen): void {
 
   round.save();
 
-  const lottery = Lottery.load(lotteryId);
+  const lottery = Lottery.load(event.params.lotteryId);
   if (lottery) {
     lottery.lastRoundId = lottery.nextRoundId;
-    lottery.nextRoundId = roundId.toString();
+    lottery.nextRoundId = event.params.roundId.toString();
     lottery.save();
   }
 }
 
 export function handleRoundReward(event: RoundReward): void {
-  const { roundId } = event.params;
-  const round = Round.load(roundId.toString());
+  const round = Round.load(event.params.roundId.toString());
   if (round) {
     round.status = "Reward";
     round.save();
@@ -114,9 +97,7 @@ export function handleRoundReward(event: RoundReward): void {
 }
 
 export function handleTicketsPurchase(event: TicketsPurchase): void {
-  const { roundId, buyer, ticketIds, amount } = event.params;
-
-  const userId = buyer.toHex();
+  const userId = event.params.buyer.toHex();
   let user = User.load(userId);
   if (user == null) {
     user = new User(userId);
@@ -127,22 +108,21 @@ export function handleTicketsPurchase(event: TicketsPurchase): void {
     user.timestamp = event.block.timestamp;
     user.save();
   }
-  user.totalTickets = user.totalTickets.plus(BigInt.fromI32(ticketIds.length));
-  user.totalAmount = user.totalAmount.plus(amount);
+  user.totalTickets = user.totalTickets.plus(BigInt.fromI32(event.params.ticketIds.length));
+  user.totalAmount = user.totalAmount.plus(event.params.amount);
   user.save();
-  const round = Round.load(roundId.toString());
+  const round = Round.load(event.params.roundId.toString());
   if (round) {
-    round.totalTickets = round.totalTickets.plus(BigInt.fromI32(ticketIds.length));
-    round.totalAmount = round.totalAmount.plus(amount);
+    round.totalTickets = round.totalTickets.plus(BigInt.fromI32(event.params.ticketIds.length));
+    round.totalAmount = round.totalAmount.plus(event.params.amount);
     round.save();
   }
 }
 
 export function handleTicketsClaims(event: TicketsClaim): void {
-  const { claimer, amount } = event.params;
-  const user = User.load(claimer.toHex());
+  const user = User.load(event.params.claimer.toHex());
   if (user) {
-    user.totalWinAmount = user.totalWinAmount.plus(amount);
+    user.totalWinAmount = user.totalWinAmount.plus(event.params.amount);
     user.save();
   }
 }
